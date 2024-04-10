9383514bea8af5153a8eb996086f307ecf905366
zli46@student.ubc.ca <zli46@student.ubc.ca>
2024-04-10 19:00:30 +0000

    added quits and joins base model

---
+++ models/base/google_drive/base_googledrive_hrjoins.sql
@@ -0,0 +1,12 @@
+SELECT _FILE,
+    _LINE,
+    _MODIFIED AS _MODIFIED_TS,
+    _FIVETRAN_SYNCED AS _FIVETRAN_SYNCED_TS,
+    EMPLOYEE_ID,
+	HIRE_DATE,
+	NAME,
+	CITY,
+	ADDRESS,
+	TITLE,
+	ANNUAL_SALARY,
+FROM {{source('google_drive', 'hr_joins')}}
