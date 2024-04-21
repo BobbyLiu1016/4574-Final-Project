8ebecd48f37348149736b347d662255cf026fa40
Hyunjin Jun <hj2642@columbia.edu>
2024-04-21 00:28:00 +0000

    Left joined tables based on expense table to avoid missing values

--- models/analytics/daily_finances.sql
+++ models/analytics/daily_finances.sql
@@ -1,8 +1,8 @@
 WITH orders as (
     SELECT 
         order_date,
-        sum(clean_shipping_cost) daily_shipping_cost,
-        SUM(total_price) as daily_order_amt
+        ROUND(SUM(clean_shipping_cost),2) daily_shipping_cost,
+        ROUND(SUM(total_price),2) as daily_order_amt
     FROM {{ ref('int_orders_withprice') }} 
     GROUP BY 1
     )
@@ -10,7 +10,7 @@ WITH orders as (
 , refund as (
     SELECT 
         DATE(returned_at) as returned_date,
-        SUM(CASE WHEN is_refunded = 'yes' THEN total_price ELSE 0 END) as daily_refund,
+        ROUND(SUM(CASE WHEN is_refunded = 'yes' THEN total_price ELSE 0 END),2) as daily_refund,
     FROM {{ ref('base_googledrive__returns') }}  r
     JOIN {{ ref('int_orders_withprice') }}  o 
     ON r.order_id = o.order_id
@@ -20,30 +20,30 @@ WITH orders as (
 , expense as (
     SELECT
         DATE,
-        SUM(clean_expense_amount) AS daily_expense
+        ROUND(SUM(clean_expense_amount),2) AS daily_expense
     FROM {{ ref('base_googledrive__expense') }} 
     GROUP BY 1
     )
 
 , hr_cost as (
-    SELECT ROUND(SUM((annual_salary/365) * days_employed)/365, 0) as DAILY_AVG_SALARY
+    SELECT ROUND(SUM((annual_salary/365) * days_employed)/365, 2) as DAILY_AVG_SALARY
     FROM {{ ref('dim_employee') }} 
     )
 
 SELECT
-    o.order_date as _date,
-    MAX(o.daily_order_amt) as daily_order_amt,
-    MAX(o.daily_shipping_cost) as daily_shipping_cost,
-    MAX(r.daily_refund) as daily_refund,
-    MAX(e.daily_expense) as daily_expense,
-    MAX(h.daily_avg_salary) as daily_avg_salary,
-    MAX(o.daily_order_amt) - MAX(r.daily_refund) - MAX(e.daily_expense) - MAX(h.daily_avg_salary) as daily_profit
-FROM orders o
-FULL OUTER JOIN refund r ON o.order_date = r.returned_date
-FULL OUTER JOIN expense e ON o.order_date = e.date
+    e.date as _date,
+    COALESCE(MAX(o.daily_order_amt),0) as daily_order_amt,
+    COALESCE(MAX(o.daily_shipping_cost),0) as daily_shipping_cost,
+    COALESCE(MAX(r.daily_refund),0) as daily_refund,
+    COALESCE(MAX(e.daily_expense),0) as daily_expense,
+    COALESCE(MAX(h.daily_avg_salary),0) as daily_avg_salary,
+    ROUND(COALESCE(MAX(o.daily_order_amt),0) - COALESCE(MAX(r.daily_refund),0) - COALESCE(MAX(e.daily_expense),0) - COALESCE(MAX(h.daily_avg_salary),0),2) as daily_profit
+FROM expense e
+LEFT JOIN orders o ON o.order_date = e.date
+LEFT JOIN refund r ON e.date = r.returned_date
 , hr_cost h
-GROUP BY _date
-ORDER BY _date ASC
+GROUP BY e.date
+ORDER BY e.date ASC
 
 
 
