64544421a2f35251cfaa5604ed4ec19e877f2e1e
Deepak Surya <ds4253@columbia.edu>
2024-04-21 00:36:08 +0000

    1. Created Client table for analytics layer 2. Made changes to the intermediate layer for client tables: changes include display only the latest address

--- models/intermediate/int_clean_client.sql
+++ models/intermediate/int_clean_client.sql
@@ -1,9 +1,19 @@
 
 
+WITH ranking AS (
+    SELECT *,
+           ROW_NUMBER() OVER(PARTITION BY o.session_id ORDER BY o.ORDER_AT_TS DESC) AS row_n
+    FROM {{ ref('base_webschema_orders')}} o
+    JOIN {{ ref('int_clean_sessions')}} s
+    ON o.session_id = s.session_id
+)
 SELECT CLIENT_ID, CLIENT_NAME, PHONE, SHIPPING_ADDRESS as ADDRESS, STATE, OS, IP
-FROM {{ ref('base_webschema_orders')}}
-JOIN {{ ref('base_webschema_sessions')}}
-ON {{ ref('base_webschema_orders')}}.SESSION_ID = {{ ref('base_webschema_sessions')}}.SESSION_ID
+FROM ranking
+WHERE row_n = 1
+
+
+
+
 
     
 
